e633095c6b59114c1f685d4aac10a5dcd8ea1cc2
Shirshak <8097377+shirshak55@users.noreply.github.com>
2020-08-15 13:48:48 +0545

    integrate clap

---
+++ src/cli.rs
@@ -0,0 +1,75 @@
+use clap::{App, Arg};
+use once_cell::sync::Lazy;
+
+#[derive(Debug)]
+pub struct Config {
+    pub run_as_service: bool,
+    pub register_service: bool,
+    pub port: u16,
+    pub html_path: String,
+}
+
+pub fn get_config() -> &'static Config {
+    static CFG: Lazy<Config> = Lazy::new(|| create_config());
+
+    &*CFG
+}
+
+pub fn create_config() -> Config {
+    let app = create_app();
+    let matches = app.get_matches();
+
+    let run_as_service = cfg!(windows) && matches.is_present("run_as_service");
+    let register_service = cfg!(windows) && matches.is_present("register_service");
+
+    Config {
+        run_as_service,
+        register_service,
+        port: matches
+            .value_of("port")
+            .and_then(|v| v.parse().ok())
+            .unwrap_or(1729),
+
+        html_path: matches
+            .value_of("html_path")
+            .and_then(|v| Some(v.to_owned()))
+            .unwrap_or_else(|| "/Users/quantum/Desktop/code/shopee/out".to_owned()),
+    }
+}
+
+fn create_app() -> App<'static, 'static> {
+    let mut app = App::new("Shopee Log Collector".to_owned())
+        .version("1.0.0")
+        .about("Shopee Log Collector")
+        .arg(
+            Arg::with_name("v")
+                .short("v")
+                .multiple(true)
+                .help("Set level of verbosity"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .required(true)
+                .help("Set level of verbosity"),
+        )
+        .arg(
+            Arg::with_name("html_path")
+                .required(true)
+                .help("Set level of verbosity"),
+        );
+
+    if cfg!(windows) {
+        app = app.arg(
+            Arg::with_name("run_as_service")
+                .long("run-as-service")
+                .help("Run as a system service. On Windows this option must be used when running a system service"),
+        ).arg(
+            Arg::with_name("register_service")
+                .long("register-service")
+                .help("Register itself as a system service"),
+        )
+    }
+
+    app
+}
--- src/main.rs
+++ src/main.rs
@@ -2,89 +2,39 @@ use tracing_subscriber::fmt::format::FmtSpan;
 use warp::Filter;
 
 #[cfg(windows)]
-use std::ffi::OsString;
-#[cfg(windows)]
-use std::time::Duration;
-#[cfg(windows)]
-use windows_service::service;
-#[cfg(windows)]
-use windows_service::service_control_handler;
+mod system_service;
 
-#[cfg(windows)]
-windows_service::define_windows_service!(service_entry_point, service_main);
-
-#[cfg(windows)]
-const SERVICE_NAME: &str = "Shopee Log Collector";
-#[cfg(windows)]
-const SERVICE_TYPE: service::ServiceType = service::ServiceType::OWN_PROCESS;
-
-#[cfg(windows)]
-fn service_main(_arguments: Vec<OsString>) {
-    let event_handler =
-        move |control_event| -> service_control_handler::ServiceControlHandlerResult {
-            match control_event {
-                service::ServiceControl::Interrogate => {
-                    service_control_handler::ServiceControlHandlerResult::NoError
-                }
-                service::ServiceControl::Stop => {
-                    service_control_handler::ServiceControlHandlerResult::NoError
-                }
-                _ => service_control_handler::ServiceControlHandlerResult::NotImplemented,
-            }
-        };
-
-    let status_handle = service_control_handler::register(SERVICE_NAME, event_handler).unwrap();
-
-    let next_status = service::ServiceStatus {
-        service_type: SERVICE_TYPE,
-        current_state: service::ServiceState::Running,
-        controls_accepted: service::ServiceControlAccept::STOP,
-        exit_code: service::ServiceExitCode::Win32(0),
-        checkpoint: 0,
-        wait_hint: Duration::default(),
-        process_id: None,
-    };
-
-    status_handle.set_service_status(next_status).unwrap();
-    run_server();
-
-    // // In future we can pass oneshot channel
-    // status_handle
-    //     .set_service_status(service::ServiceStatus {
-    //         service_type: SERVICE_TYPE,
-    //         current_state: service::ServiceState::Stopped,
-    //         controls_accepted: service::ServiceControlAccept::empty(),
-    //         exit_code: service::ServiceExitCode::Win32(0),
-    //         checkpoint: 0,
-    //         wait_hint: Duration::default(),
-    //         process_id: None,
-    //     })
-    //     .unwrap();
-}
-
+pub mod cli;
 pub mod controllers;
 pub mod helpers;
 pub mod models;
 pub mod routes;
 
-#[cfg(unix)]
 fn main() {
-    run_server();
+    let config = cli::get_config();
+
+    let exit_code = match run_platform(config) {
+        Ok(_) => 0,
+        Err(error) => {
+            println!("{}", error);
+            // Todo: Log
+            1
+        }
+    };
+
+    std::process::exit(exit_code);
 }
 
-#[cfg(windows)]
-fn main() {
-    windows_service::service_dispatcher::start(SERVICE_NAME, service_entry_point).unwrap();
+#[cfg(not(windows))]
+fn run_platform(config: &cli::Config) -> Result<(), String> {
+    run_server(config)
 }
 
-fn run_server() {
+fn run_server(config: &cli::Config) -> Result<(), String> {
     let traces =
         std::env::var("RUST_LOG").unwrap_or_else(|_| "shopee_logs_collector=trace".to_owned());
 
-    let port = std::env::var("PORT")
-        .ok()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(1729);
+    let port = config.port;
 
     tracing_subscriber::fmt()
         .with_env_filter(traces)
@@ -94,12 +44,13 @@ fn run_server() {
     let db = models::blank_db();
     let api = routes::all(db);
 
-    let fs = warp::fs::dir(
-        std::env::var("HTML_DIR")
-            .unwrap_or_else(|_| "/Users/quantum/Desktop/code/shopee/out".to_owned()),
-    );
+    let fs = warp::fs::dir(config.html_path.to_owned());
+
     let routes = fs.or(api.with(warp::trace::request()));
 
-    let mut rt = tokio::runtime::Runtime::new().unwrap();
+    let mut rt = tokio::runtime::Runtime::new().map_err(|_| "Couldn't start runtime")?;
+
     rt.block_on(warp::serve(routes).run(([127, 0, 0, 1], port)));
+
+    Ok(())
 }
