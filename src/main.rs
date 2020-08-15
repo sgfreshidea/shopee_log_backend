34687332ba2cdd040add1cc1a15a510fbaff1ffb
Shirshak <8097377+shirshak55@users.noreply.github.com>
2020-08-15 16:25:34 +0545

    add info

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,9 @@ fn run_server(config: &cli::Config) -> Result<(), String> {
 
     let mut rt = tokio::runtime::Runtime::new().map_err(|_| "Couldn't start runtime")?;
 
+    println!("Listening on port {}", port);
+    println!("Serving {}", config.html_path);
+
     rt.block_on(warp::serve(routes).run(([127, 0, 0, 1], port)));
 
     Ok(())
